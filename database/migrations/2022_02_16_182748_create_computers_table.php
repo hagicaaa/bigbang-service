6fbc2f64cfa7a9828f6179cfc7057d6186b03349
Muhammad Rafi <m.rafi.shadiq@gmail.com>
2022-02-17 01:51:13 +0700

    create migration

---
+++ database/migrations/2022_02_16_182748_create_computers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComputersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('computers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('brand_id');
+            $table->string('type');
+            $table->string('serial_number');
+            $table->string('problem');
+            $table->boolean('eq_bag');
+            $table->boolean('eq_charger_cable');
+            $table->timestamps();
+
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('computers');
+    }
+}
---
+++ database/migrations/2022_02_16_183759_create_reparations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReparationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reparations', function (Blueprint $table) {
+            $table->id();
+            $table->string('inv_id');
+            $table->unsignedBigInteger('computer_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('received_by');
+            $table->timestamps();
+
+            $table->foreign('computer_id')->references('id')->on('computers');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('received_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reparations');
+    }
+}
