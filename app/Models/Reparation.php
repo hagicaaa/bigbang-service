c53b426ee2bbc18788cba30a261a4713cf4c882b
Muhammad Rafi <muhammadrafi@Muhammads-MacBook-Air.local>
2022-06-18 18:16:25 +0700

    fix models

--- app/Models/Reparation.php
+++ app/Models/Reparation.php
@@ -19,7 +19,7 @@ class Reparation extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = ['inv_id','computer_id','customer_id','inspection_date','repair_start','post_repair_inspection_date','repair_finish','received_by'];
     // protected $hidden = [];
     // protected $dates = [];
 
