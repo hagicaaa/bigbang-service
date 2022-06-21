036217bf3b800001b84e1ccc511367635dc24a83
Muhammad Rafi <muhammadrafi@Muhammads-MacBook-Air.local>
2022-06-21 19:08:06 +0700

    add rule on brand request

--- app/Http/Requests/BrandRequest.php
+++ app/Http/Requests/BrandRequest.php
@@ -26,7 +26,7 @@ class BrandRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required'
         ];
     }
 
