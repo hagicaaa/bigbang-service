ec3784086e2453c57ca9c614dd100a588d267e63
Muhammad Rafi <muhammadrafi@Muhammads-MacBook-Air.local>
2022-10-31 17:55:15 +0700

    reparations sidebar dropdown

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,14 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('reparation') }}'><i class='nav-icon la la-tools'></i> Reparations</a></li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-tools"></i> Reparations</a>
+    <ul class="nav-dropdown-items">
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reparation') }}'><i class='nav-icon la la-desktop'></i> Need Checking</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reparation') }}'><i class='nav-icon la la-address-card'></i> Ongoing Reparation</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reparation') }}'><i class='nav-icon la la-address-card'></i> Post-Reparation Checking</a></li>
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('reparation') }}'><i class='nav-icon la la-address-card'></i> Need Pickup</a></li>
+    </ul>
+</li>
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-server"></i> Master Data</a>
     <ul class="nav-dropdown-items">
